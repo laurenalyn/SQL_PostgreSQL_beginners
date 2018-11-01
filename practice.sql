cd7ddfe2c0349574bd022246587286a0195d43b7
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-01 18:04:26 -0400

    Exercise 1: Selecting from a table

--- practice.sql
+++ practice.sql
@@ -45,5 +45,26 @@ WHERE gender = 'F'
 AND first_name = 'Emily'
 AND id > 5;
 
+-- ==============================================================
+--EXERCISE 1: SELECTING FROM A TABLE
+-- ==============================================================
+--1. Select the city and postcode fromt the address table
+SELECT city, postcode FROM address;
 
+--2. Select the products name from the products table where the season is winter
 
+SELECT name FROM products
+WHERE season = 'Winter';
+
+--3. Select id, quantity and order_date from the orders table where price is more 
+--than 25.00 and quantity is less than 4
+
+SELECT id, quantity, order_date FROM orders
+WHERE price > 25.00
+AND quantity < 4;
+
+--4. Select all rows of data from the products table where season is equal to 
+--summer and wholsale_price is atleast 20.00
+SELECT * FROM products
+WHERE season = 'Summer'
+AND wholesale_price >= 20.00;
